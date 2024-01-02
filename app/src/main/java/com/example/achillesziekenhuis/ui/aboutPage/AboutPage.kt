af9e8bf0fa04c032fd6f35f1fcf4d230aeffbd8c
AchillesVan <achilles.vanbelleghem@student.hogent.be>
2024-01-02 15:48:32 +0100

    Added about page

--- app/src/main/java/com/example/achillesziekenhuis/ui/aboutPage/AboutPage.kt
+++ app/src/main/java/com/example/achillesziekenhuis/ui/aboutPage/AboutPage.kt
@@ -1,8 +1,18 @@
 package com.example.achillesziekenhuis.ui.aboutPage
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.achillesziekenhuis.ui.ZiekenhuisScaffold
 
 /**
@@ -26,8 +36,118 @@ fun AboutPage(
         goToAbout = goToAbout,
         goToLogin = goToLogin,
     ) {
-        Text(
-            text = "About",
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column {
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Email",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "info.achillesziekenhuis@imaginary.net",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+            Column {
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Telefoon",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "0456 67 89 12",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+            Column {
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Adres",
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 40.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Ziekenhuisstraat 1",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "8920 Ieper",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        fontSize = 20.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Dit zijn fictieve gegevens",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 8.dp),
+                        fontSize = 12.sp,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+            }
+
+        }
     }
 }
